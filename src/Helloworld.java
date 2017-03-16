19c1af947971313ef83f6650d48bc6d762b706d0
sen <yangwangsen_@yeah.net>
2017-03-16 11:34:38 +0800

    方法

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,7 +1,16 @@
 
 public class Helloworld {
-  public static void main(String[] args) {
+private int a;
 
+
+  public void  add{
+	a++;
+}
+public void select{
+
+}
+  public static void main(String[] args) {
+	
 
 
     System.out.println("123456");
